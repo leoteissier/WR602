f2b7ae99cb15338a15e787972014c2399406a639
Léo Teissier <100595766+leoteissier@users.noreply.github.com>
2024-02-05 16:09:52 +0100

    feat : make-entities

---
+++ src/Entity/Pdf.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PdfRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\HasLifecycleCallbacks] // This annotation is used to call the setCreatedAt() method before the entity is persisted and updated.
+#[ORM\Entity(repositoryClass: PdfRepository::class)]
+#[ORM\Table(name: '`pdf`')]
+class Pdf
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_IMMUTABLE, nullable: true)]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'pdfs')]
+    private ?User $user_id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    #[ORM\PrePersist] // This annotation is used to call the setCreatedAt() method before the entity is persisted.
+    public function setCreatedAt(): static
+    {
+        if ($this->createdAt === null) { // Ne définir createdAt que s'il est null
+            $this->createdAt = new \DateTimeImmutable();
+        }
+
+        return $this;
+    }
+
+    public function getUserId(): ?User
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(?User $user_id): static
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+}
